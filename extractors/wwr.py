4a63babd44275bf71cdbba2f3db4aa5d21aabc0d
didmaehd@gmail.com <didmaehd@gmail.com>
2022-11-04 00:42:47 +0900

    20221103#01

---
+++ extractors/wwr.py
@@ -0,0 +1,36 @@
+
+from bs4 import BeautifulSoup
+from requests import get
+
+def extract_wwr_job(keyword):
+    base_url = "https://weworkremotely.com/remote-jobs/search?&term="
+
+    reponse = get(f"{base_url}{keyword}")
+    if reponse.status_code != 200:
+        print ("We could not find website")
+    else : # 웹사이트가 존재 한다면
+        results = [] # 리스트를 만들고
+        soup = BeautifulSoup(reponse.text, "html.parser") #웹 사이트의 html을 beautifulsoup를 이용해서 변수에 담고
+        jobs = (soup.find_all("section", class_="jobs")) # class가 jobs인 section을 변수에 담고
+        for job_selction in jobs:
+            job_post = job_selction.find_all("li")
+            job_post.pop(-1) # job변수의 li를 job_post에 담는데 마지막 하나는 제외한다
+            # for job_title in job_post:
+            #     title = job_title.find_all(class_="title")
+            #     print(title)
+            for post in job_post:
+                anchors = post.find_all("a") # job_post 의 a 태그를 변수에 담고
+                #anchor.pop(0)
+                anchor = anchors[1] #첫번쨰것만 다시 변수에 담고
+                link = anchor["href"] #  
+                company,kind,region = anchor.find_all("span",class_="company") #각각의 값을 각각의 변수에 넣고
+                title = anchor.find("span", class_="title") # title을 변수에 넣고
+                job_data = {
+                    "link" : f"https://weworkremotely.com/{link}",
+                    "company" : company.string,
+                    "region" : region.string,
+                    "posion" : title.string,
+                } # 필요한 값의 string값만 dictionary에 저장
+                results.append(job_data) # add dictionary data to result list 
+        return results
+
