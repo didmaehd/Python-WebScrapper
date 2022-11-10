e8bd05507e092f4158e84187d25ef1664c3605e5
didmaehd@gmail.com <didmaehd@gmail.com>
2022-11-11 01:45:09 +0900

    20221110#01

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from bs4 import BeautifulSoup
 from requests import get
 from extractors.wwr import extract_wwr_job
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+Options = Options()
+# Options.add_argument("--no-sandbox")
+# Options.add_argument("--disable-dev-shm-usage")
+
+browser = webdriver.Chrome(options=Options)
+browser.get("https://kr.indeed.com/jobs?q=python&limit=50")
+
+soup = BeautifulSoup(browser.page_source,"html.parser")
+job_list = soup.find("ul", class_="jobsearch-ResultsList")
+jobs = job_list.find_all("li", recursive=False)
 
